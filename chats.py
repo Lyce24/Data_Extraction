b73de7b2a372824f95f30c5c50811147d929b0b6
Yancheng Liu <89893105+Lyce24@users.noreply.github.com>
2022-03-22 13:13:50 -0700

    Update chats.py

--- chats.py
+++ chats.py
@@ -1,4 +1,11 @@
 # chats.py
+# Process a text and distill statistics about the bi-gram and tri-gram 
+# word occurrences in the entered text. It does this by using a dictionary 
+# of words and bi-grams. Each dictionary entry gives the list of words in 
+# the text that follow that word/bi-gram (and possibly a count of the number 
+# of times each follower does so). The code then generates random text based on 
+# these statistics.
+#
 # Usage: python3 chats.py
 #
 # This command processes a series of lines of text, looking for
